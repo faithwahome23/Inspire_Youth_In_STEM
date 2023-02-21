6eeae569d363e7358b92fcb8458fbe6cb41f4886
faithwahome <wahomewarish@gmail.com>
2023-02-21 10:42:50 +0300

    main

---
+++ Week-3/break.py
@@ -0,0 +1,10 @@
+#break keywork in python
+num = 0
+while(num < 10):
+    print(num)
+    if(num == 5): 
+       break
+    print("braking at...5")
+    num = num + 1
+#write a progeamme that solves quadratic equation
+#using for loop draw a diamond,triangle,pascals triangle
---
+++ Week-3/loops.py
@@ -0,0 +1,20 @@
+#do while
+
+#while(True):
+   #  print("My name is Faith")
+
+
+
+number = 24
+while(True):
+    if(num > 20):
+        print(num)
+age = 24 
+gender = "male"
+if ( age < 18 & gender ("male")):
+  if (weight > 75 | height < 125):
+    print("You are obese")
+   elif 
+    print("You are healthy")
+elif
+    print("You are just fine") 
